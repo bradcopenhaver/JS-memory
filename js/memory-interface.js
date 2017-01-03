c2a9bf067254a1534f8b3d83d17846911516445e
Brad Copenhaver and Steve Burton <brad+steve@>
2017-01-02 16:46:43 -0800

    selector to show images not working

--- js/memory-interface.js
+++ js/memory-interface.js
@@ -5,7 +5,16 @@ $(document).ready(function() {
     var game = new Game();
     game.createBoard();
 
+    var gridHtml = "";
     for (i=0; i<16; i++){
-      $("#"+i).html("<img src=./img/"+game.board[i].image+".jpg>");
+      gridHtml = gridHtml.concat("<div class = cardBorder id ="+ i +"><img id=img"+i+ " src=./img/"+game.board[i].image+".jpg></div> ");
+    }
+    $("#grid").html(gridHtml);
+
+    for(i=0; i<16; i++){
+      $("#"+ i).click(function(){
+        var thisId = "#img"+ i;
+        $(thisId).show();
+      });
     }
 });
